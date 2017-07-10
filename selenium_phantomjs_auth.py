db4b57da7de35fc84fe2c18006f02723aa688424
Germey <cqc@cuiqingcai.com>
2017-07-10 21:42:37 +0800

    add auth

--- selenium_phantomjs_auth.py
+++ selenium_phantomjs_auth.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 
 service_args = [
     '--proxy=127.0.0.1:9743',
-    '--proxy-type=http'
+    '--proxy-type=http',
+    '--proxy-auth=username:password'
 ]
 
 browser = webdriver.PhantomJS(service_args=service_args)
